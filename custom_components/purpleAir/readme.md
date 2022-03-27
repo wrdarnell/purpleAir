6d2ebd95e86eba5236a611240db0e33024e47187
Will Darnell <will@willdarnell.net>
2022-03-26 17:32:25 -0700

    initial scaffolded sensor

---
+++ custom_components/purpleAir/readme.md
@@ -0,0 +1,7 @@
+# PurpleAir Monitoring Component
+
+## Overview
+Add overview here
+
+## Installation
+Add installation instructions here
---
+++ custom_components/purpleAir/sensor.py
@@ -0,0 +1,41 @@
+"""Platform for PurpleAir sensor integration."""
+from __future__ import annotations
+
+from homeassistant.components.sensor import (
+	SensorDeviceClass,
+	SensorEntity,
+	SensorStateClass,
+)
+from homeassistant.const import CONCENTRATION_MICROGRAMS_PER_CUBIC_METER
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
+
+def setup_platform(
+	hass: HomeAssistant,
+	config: ConfigType,
+	add_entities: AddEntitiesCallback,
+	discovery_info: DiscoveryInfoType | None = None
+) -> None:
+	"""Set up the sensor platform."""
+	add_entities([PurpleAirSensor()])
+
+
+class PurpleAirSensor(SensorEntity):
+	"""Representation of a Sensor."""
+
+	_attr_name = "PM 2.5"
+	_attr_native_unit_of_measurement = CONCENTRATION_MICROGRAMS_PER_CUBIC_METER
+	_attr_device_class = SensorDeviceClass.PM25
+	_attr_state_class = SensorStateClass.MEASUREMENT
+
+	def update(self) -> None:
+		"""Fetch new state data for the sensor.
+
+		This is the only method that should fetch new data for Home Assistant.
+		"""
+		self._attr_native_value = self.readSensor()
+		
+	def readSensor(self) -> int:
+		return 42

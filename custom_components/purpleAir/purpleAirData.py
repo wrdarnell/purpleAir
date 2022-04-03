198bb80282b7b060ea0323ae101221f356e026f4
Will Darnell <will@willdarnell.net>
2022-04-02 23:28:20 -0700

    Cleanup, comments

--- custom_components/purpleAir/purpleAirData.py
+++ custom_components/purpleAir/purpleAirData.py
@@ -2,9 +2,10 @@ import sched, time
 import requests
 
 class conditionInfo():
+	"""Helper class containing info about the monitored condition"""
 	def __init__(self, func, decimalPlaces = 0):
-		self._func          = func
-		self._decimalPlaces = decimalPlaces
+		self._func          = func          # Function to call to set a value based on the json pulled form the sensor
+		self._decimalPlaces = decimalPlaces # Number of decimal places for any rounding
 
 	@property
 	def decimalPlaces(self):
@@ -15,38 +16,38 @@ class conditionInfo():
 		return self._func
 
 class purpleAirData():
+	"""Class that pulls data from the PurpleAir sensor and updates values for monitored conditions"""
 	def __init__(self, url, freq, conditions):
-		self._url     = url
-		self._freq    = freq
-		self._sched   = sched.scheduler(time.time, time.sleep)
-		self._values  = {}
+		self._url     = url  # URL of the PA sensor (http://foo.bar/json)
+		self._freq    = freq # Frequency to update
+		self._values  = {}   # Dict of values for the current reading
+
+		# Init the list of values for the conditions we monitor
 		for condition in conditions:
-			self._values[condition] = 0.0
+			self._values[condition] = 0.0 # Assume 0. May want to investigate defaults based on type or condition.
 
 	def refreshData(self):
-		response  = requests.get(self._url)
-		json      = response.json()
+		"""Get data from the PA sensor and update monitored conditions"""
+		response  = requests.get(self._url)	# Read the sensor
+		json      = response.json()         # Parse the JSON
 		
+		# Go update the conditions we were asked to monitor
 		for condition in self.conditions:
-			self.setData(json, condition)
-
-	def setData(self, json, condition):
-		info = self.conditions[condition]
-		info.func(self, json, condition)
-
-	def getData(self, condition):
-		return self._conditions[condition]
+			self.conditions[condition].func(self, json, condition)
 		
 	def sensorAvg(self, json, condition):
+		"""Sets a value based on the average of the A and B sensor. Assumes 'foo' and 'foo_b' in json"""
 		info      = self.conditions[condition]
-		readings  = self.pairedReadings(json, condition)
-		sensorAvg = round(sum(readings) / len(readings), info.decimalPlaces)
-		self.setValue(condition, sensorAvg)
+		readings  = self.pairedReadings(json, condition) # Get the A and B values
+		sensorAvg = round(sum(readings) / len(readings), info.decimalPlaces) # Compute the average and round
+		self.setValue(condition, sensorAvg) # Post it
 	
 	def singleValue(self, json, condition):
+		"""Sets a value based on a single key in the json object"""
 		self.setValue(condition, json[condition])
 	
 	def pairedReadings(self, json, condition):
+		"""Returns a list with the A and B values for a condition (e.g., 'foo' and 'foo_b')"""
 		prefix = condition
 		return [json[prefix], json[prefix + '_b']]
 	
@@ -54,10 +55,13 @@ class purpleAirData():
 		self._values[condition] = value
 
 	def health(self, json, condition):
+		"""Computes and sets a synthetic health percentage by comparing A and B particle counters"""
+		# TODO: Find a better algorithm
 		readings = self.pairedReadings(json, 'pm2_5_atm')
 		self.setValue(condition, readings[1] / readings[0])
 
-	conditions = {
+	### Master list of conditions that this implementation can monitor with helper object for how to process the metric
+	conditions = { 
 			  'pm2_5_atm'         : conditionInfo(func=sensorAvg,   decimalPlaces=1)
 			, 'pm10_0_atm'        : conditionInfo(func=sensorAvg,   decimalPlaces=1)
 			, 'pm2.5_aqi'         : conditionInfo(func=sensorAvg,   decimalPlaces=0)

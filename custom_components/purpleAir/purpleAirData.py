208b7633c679c9760506210995b4b15bf4246f67
Will Darnell <will@willdarnell.net>
2022-04-09 16:57:38 -0700

    progress

--- custom_components/purpleAir/purpleAirData.py
+++ custom_components/purpleAir/purpleAirData.py
@@ -18,9 +18,9 @@ class conditionInfo():
 class purpleAirData():
 	"""Class that pulls data from the PurpleAir sensor and updates values for monitored conditions"""
 	def __init__(self, url, freq, conditions):
-		self._url     = url  # URL of the PA sensor (http://foo.bar/json)
-		self._freq    = freq # Frequency to update
-		self._values  = {}   # Dict of values for the current reading
+		self._url    = url  # URL of the PA sensor (http://foo.bar/json)
+		self._freq   = freq # Frequency to update
+		self._values = {}   # Dict of values for the current reading
 
 		# Init the list of values for the conditions we monitor
 		for condition in conditions:
@@ -33,32 +33,28 @@ class purpleAirData():
 		
 		# Go update the conditions we were asked to monitor
 		for condition in self.conditions:
-			self.conditions[condition].func(self, json, condition)
+			self._values[condition] = self.conditions[condition].func(self, json, condition)
 		
-	def sensorAvg(self, json, condition):
+	def sensorAvg(self, json, condition) -> float:
 		"""Sets a value based on the average of the A and B sensor. Assumes 'foo' and 'foo_b' in json"""
 		info      = self.conditions[condition]
 		readings  = self.pairedReadings(json, condition) # Get the A and B values
-		sensorAvg = round(sum(readings) / len(readings), info.decimalPlaces) # Compute the average and round
-		self.setValue(condition, sensorAvg) # Post it
+		return round(sum(readings) / len(readings), info.decimalPlaces) # Compute the average and round
 	
 	def singleValue(self, json, condition):
 		"""Sets a value based on a single key in the json object"""
-		self.setValue(condition, json[condition])
+		return json[condition]
 	
 	def pairedReadings(self, json, condition):
 		"""Returns a list with the A and B values for a condition (e.g., 'foo' and 'foo_b')"""
 		prefix = condition
 		return [json[prefix], json[prefix + '_b']]
 	
-	def setValue(self, condition, value):
-		self._values[condition] = value
-
-	def health(self, json, condition):
+	def health(self, json, condition) -> float:
 		"""Computes and sets a synthetic health percentage by comparing A and B particle counters"""
 		# TODO: Find a better algorithm
 		readings = self.pairedReadings(json, 'pm2_5_atm')
-		self.setValue(condition, readings[1] / readings[0])
+		return readings[0] / readings[1]
 
 	### Master list of conditions that this implementation can monitor with helper object for how to process the metric
 	conditions = { 
